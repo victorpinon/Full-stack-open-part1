2cef0457fb0a7fefbee23acac0bfcd9dc5a54b16
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-05 12:13:41 +0100

    exercise 1.7

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -44,14 +44,24 @@ const Feedback = ({handleGood, handleNeutral, handleBad}) => {
   )
 }
 
-const Statistics = ({good, neutral, bad}) => (
-  <div>
+const Statistics = ({good, neutral, bad}) => {
+  const all = good+neutral+bad
+  let average = ((good-bad)/all).toFixed(2)
+  if (isNaN(average)) average = 0
+  let positive = (good*100/all).toFixed(1)
+  if (isNaN(positive)) positive = 0
+  return (
+    <div>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div> 
-)
+  )
+}
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
