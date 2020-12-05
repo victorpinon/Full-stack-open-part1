fb316661d730795df0e127f22d14ce3b1b5571a5
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-05 12:37:39 +0100

    ex 1.11

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -19,7 +19,11 @@ const Feedback = ({handleGood, handleNeutral, handleBad}) => {
 }
 
 const Statistic = ({text, value}) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+  
 )
 
 const Statistics = ({good, neutral, bad}) => {
@@ -38,12 +42,16 @@ const Statistics = ({good, neutral, bad}) => {
     return (
       <div>
         <h1>statistics</h1>
-        <Statistic text="good" value={good}/>
-        <Statistic text="neutral" value={neutral}/>
-        <Statistic text="bad" value={bad}/>
-        <Statistic text="all" value={all}/>
-        <Statistic text="average" value={average}/>
-        <Statistic text="positive" value={positive}/>
+        <table>
+          <tbody>
+            <Statistic text="good" value={good}/>
+            <Statistic text="neutral" value={neutral}/>
+            <Statistic text="bad" value={bad}/>
+            <Statistic text="all" value={all}/>
+            <Statistic text="average" value={average}/>
+            <Statistic text="positive" value={positive}/>
+          </tbody>
+        </table>
       </div> 
     )
   }
